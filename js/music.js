ac2614d22294b70210302f3b8f4b43307760d672
Xabi Errotabehere <github@keinu.net>
2015-10-31 18:33:00 +1100

    Output removed

--- js/music.js
+++ js/music.js
@@ -13,8 +13,6 @@
 
 		favourites.result.forEach(function(album) {
 
-			console.log(album);
-
 			var albumContainer = document.createElement("div");
 				albumContainer.setAttribute("class", "album");
 
@@ -32,11 +30,6 @@
 				label.innerHTML = album.name + " by " + album.artist;
 
 			albumContainer.appendChild(label);
-
-			// var player = document.createElement("iframe");
-			// 	player.setAttribute("src", album.embedUrl);
-
-			// image.appendChild(player);
 
 			document.getElementById("images").appendChild(albumContainer);
 
